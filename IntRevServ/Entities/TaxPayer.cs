857a3b98e851c8630ab8f861d1a6d1680b48cf30
4cruzeta <4cruzetam@gmail.com>
2019-11-21 14:02:39 -0300

    primeiro Commit

---
+++ IntRevServ/Entities/TaxPayer.cs
@@ -0,0 +1,20 @@
+﻿namespace IntRevServ.Entities
+{
+    abstract class TaxPayer
+    {
+        public string Name { get; set; }
+        public double AnualIncome { get; set; }
+
+        protected TaxPayer()                            // Corrigida linha de public para protected
+        {                                               // Construtor de classe abstrata não deve ser
+        }                                               // público.
+
+        protected TaxPayer(string name, double anualIncome)
+        {
+            Name = name;
+            AnualIncome = anualIncome;
+        }
+
+        public abstract double Tax();
+    }
+}
